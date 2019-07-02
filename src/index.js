3fff90e6a20506da6e6082f096f2216bf32b260e
arvindeva <arvindeva@gmail.com>
2019-07-02 14:27:06 +0700

    index

--- src/index.js
+++ src/index.js
@@ -75,7 +75,7 @@ const eraseDatabaseOnSync = true;
 
 sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
   if (eraseDatabaseOnSync) {
-    seedDb(new Date);
+    seedDb(new Date());
   }
 
   app.listen({ port: 4000 }, () => {
